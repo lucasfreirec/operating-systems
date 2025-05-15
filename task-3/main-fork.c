f74cb87b008b9acb8144bc392a48def71549f779
lucasfreirec <lucasfreirecosta@hotmail.com>
2025-05-15 16:57:24 -0300

    Update files

--- task-3/main-fork.c
+++ task-3/main-fork.c
@@ -3,16 +3,17 @@
 
 int main() 
 {
+
 	int pid = fork();
 
 	if (pid==0){
-		printf("\n\nProcesso filho\n");
+		printf("\nProcesso filho\n");
 
 		printf("PID: %d\n", getpid());
 		printf("PID do pai: %d\n", getppid());
 	} 
 	else {
-		printf("\n\nProcesso pai\n");
+		printf("\nProcesso pai\n");
 		printf("PID: %d\n", getpid());
 	}
 	
